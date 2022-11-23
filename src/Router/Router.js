71f5b17a4c9db12a7b2de6dd515105b8ed584cc4
arifusinbad <ariful.islam.sinbad@gmail.com>
2022-11-24 02:08:20 +0600

    commit 3

---
+++ src/Router/Router.js
@@ -0,0 +1,15 @@
+const { createBrowserRouter } = require("react-router-dom");
+const { default: Main } = require("../Layout/Main");
+const { default: Home } = require("../Page/Home");
+
+const router = createBrowserRouter([
+ {
+  path:'/', element:<Main></Main>,
+  children:[
+   {
+    path:'/', element:<Home></Home>
+   }
+  ]
+ }
+]);
+export default router;
